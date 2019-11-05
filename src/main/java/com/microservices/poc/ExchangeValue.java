8e73ecc07552938dc23fff149f63959367f89f89
Seetaram <31759014+seetaramtaduri@users.noreply.github.com>
2019-11-05 16:32:51 +0530

    currency exchange service

---
+++ src/main/java/com/microservices/poc/ExchangeValue.java
@@ -0,0 +1,72 @@
+package com.microservices.poc;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	@Id
+    private Long id;
+    private BigDecimal conversionMultiple;
+    private int port;
+	public ExchangeValue() {
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public ExchangeValue(Long id,String from, String to,  BigDecimal conversionMultiple) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.id = id;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	 
+
+}
